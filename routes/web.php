e140c67d7f4e481cabc684cb77e59c662f2dc097
DaskyP <danteyahir123@gmail.com>
2024-11-14 22:27:58 -0600

    change: breeze starter kit

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/taller/{post}',function($post){//recibe una variable que toma el parametro de ruta
-    return "{$post}";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/home',[HomeController::class,'index']);
-
-
-
-Route::view('/login',"login")->name('login');
-Route::view('/registro', "register")->name('registro');
-Route::view('/privada', "secret")->middleware('auth')->name('privada'); //vamos a anadir un middleware para validar 
-//lo que hace es buscar si las credenciales del usuario son correctas, si no pss vas pal login
-
-Route::post('/validar-registro', [LoginController::class, 'register'])-> name('validar-registro');
-Route::post('/inicia-sesion', [LoginController::class, 'login'])->name('inicia-sesion');
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-
+require __DIR__.'/auth.php';
